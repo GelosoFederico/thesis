42ff5a5e9db88b28b09a9f634525cdab3aaedab2
GelosoFederico <gelosofederico@gmail.com>
2021-05-15 20:31:44 -0300

    Started to code the  two phase topology recovery

--- GrotasAlgorithm.py
+++ GrotasAlgorithm.py
@@ -4,6 +4,32 @@
 import pandapower
 import pandapower.networks
 import numpy as np
+import scipy.linalg
+
+# TODO change all np.transpose to matrix.T
+
+def two_phase_topology_recovery(N,M,U,sigma_theta_tilde, sigma_p, sigma_noise_approx):
+    # Step 1) Get reduced sample covariance matrix
+    sigma_p_tilde =  np.transpose(U) @ sigma_p @ U 
+    U_pseudinv = np.linalg.pinv(U)
+
+    # Step 2) Get optimal solution
+    # one way to get square root
+    # TODO analize if the square root algorithm used here is useful
+    sigma_theta_tilde_sqrt = scipy.linalg.sqrtm(sigma_theta_tilde)
+
+    # another way to get square root
+    # TODO check if I whould always send hermitian
+    # u_sigma, s_sigma, v_sigma = np.linalg.svd(sigma_theta_tilde,compute_uv=True, hermitian=True)
+
+    sigma_theta_tilde_sqrt_inv = np.linalg.inv(sigma_theta_tilde_sqrt)
+
+    aux = sigma_theta_tilde_sqrt @ (sigma_p_tilde - sigma_noise_approx**2 * U_pseudinv @ U_pseudinv.T ) @ sigma_theta_tilde_sqrt
+    B_estimated = sigma_theta_tilde_sqrt_inv @ aux @ sigma_theta_tilde_sqrt_inv
+
+    # TODO use CVXPY to solve the optimization
+    return B_estimated
+
 
 net = pandapower.networks.mv_oberrhein()
 
@@ -14,14 +40,14 @@ net = pandapower.networks.mv_oberrhein()
 
 # net.bus contains all buses
 N = 10
-normal_variance = 0.01
+noise_sigma = 0.1
 
 pandapower.runpp(net)
 actual_p = net.res_bus['p_mw']
 M = len(actual_p)
 actual_p = np.tile(np.array(actual_p),(N,1))
 
-noise =  np.random.default_rng().normal(0, normal_variance, actual_p.shape)
+noise =  np.random.default_rng().normal(0, noise_sigma**2, actual_p.shape)
 measurements = actual_p + noise
 print(measurements)
 
@@ -60,13 +86,29 @@ print(E_p)
 # Step 3-4) Get eigenvalues and estimate noise variance with the smallest eigenvalue
 
 eigenvalues = np.linalg.eig(E_p)[0]
-sigma = eigenvalues.min()
+sigma_noise_approx = eigenvalues.min()
 
 # Step 5) Get \hat(\tilde(B)). The paper explains two methods to approximate maximum likelihood B
 
-B_tilde_aprox = np.eye(M)
+B_tilde_approx = two_phase_topology_recovery(N,M,U,E_theta_tilde,E_p,sigma_noise_approx)
 
+# TODO this steps (6 7 8) are all untested
 # Step 6) Get \hat(B) from \hat(\tilde(B))
 
-B_aprox = U @ B_tilde_aprox @ np.transpose(U)
+B_approx = U @ B_tilde_approx @ np.transpose(U)
+
+# Step 7) Impose sparsity with a threshold
+sparsity_value = 1/M
+threshold = np.diag(B_approx).min() * sparsity_value
+B_approx = np.where(np.abs(B_approx) < threshold, 0, B_approx)
+
+# Step 8) Evaluate theta
+
+theta_approx = np.zeros_like(measurements)
+for i in range(0, measurements.shape[0]):
+    theta_approx[i,:] = E_theta @ B_approx @ np.linalg.pinv(np.transpose(B_approx) @ E_theta @ B_approx + sigma_noise_approx * np.eye(M)) @ measurements[i]
+print(theta_approx)
+
+
+
 
