fb328d4e20f3f6fa0cb18e2087f0f32c04bf2473
GelosoFederico <gelosofederico@gmail.com>
2021-10-06 23:06:45 -0300

    Plot changes

--- GrotasTests.py
+++ GrotasTests.py
@@ -8,8 +8,8 @@ from utils import matprint, get_U_matrix
 from simulations import F_score, cramer_rao_bound, MSE_matrix, get_observations
 from GrotasAlgorithm import GrotasAlgorithm
 
-two_phase_enabled = False
-augmented_enabled = True
+two_phase_enabled = True
+augmented_enabled = False
 
 net = pandapower.networks.case14()
 pandapower.runpp(net)
@@ -21,9 +21,14 @@ print("B_Real_tilde")
 B_real_tilde = B_real[1:,1:]
 matprint(B_real_tilde)
 c = 1
-range_SNR = np.linspace(0, 25, 10)
+range_SNR = np.linspace(0, 25, 6)
+points = [200, 1500]
 for SNR in range_SNR:
-    for N in [1500]:
+    for N in points:
+        sigma_est = None
+        sigma_est_aug = None
+        sigma_p = None
+        sigma_p_aug = None
         observations, sigma_theta = get_observations(N, SNR, c, B_real)
         
         if two_phase_enabled:
@@ -63,50 +68,72 @@ for SNR in range_SNR:
                 "F_score": fs_aug
             })
 
-        if two_phase_enabled:
-            M = B.shape[0]
-            U = get_U_matrix(M)
-            CRB = cramer_rao_bound(M, U.T @ B @ U, sigma_est**2,sigma_p, U.T @ sigma_theta @ U, N)
-            MSE_tests.append({
-                "method": 'cramer_rao_bound',
-                "N": N,
-                "SNR": SNR,
-                "MSE": CRB,
-            })
-
-if two_phase_enabled:
-    MSE_two_phase = [x for x in MSE_tests if x['method']=='two_phase_topology']
-    MSE_two_phase_for_plot = [x['MSE'] for x in MSE_two_phase]
-    fscore_for_plot = [x['F_score'] for x in MSE_two_phase]
-
-if augmented_enabled:
-    MSE_augmented_lagrangian = [x for x in MSE_tests if x['method']=='augmented_lagrangian']
-
-    MSE_augmented_lagrangian_for_plot = [x['MSE'] for x in MSE_augmented_lagrangian]
-    fscore_augmented_lagrangian_for_plot = [x['F_score'] for x in MSE_augmented_lagrangian]
-
-if two_phase_enabled:
-    CRBs = [x for x in MSE_tests if x['method']=='cramer_rao_bound']
-    CRBs_for_plot = [x['MSE'] for x in CRBs]
-
+        M = B.shape[0]
+        U = get_U_matrix(M)
+        sigma_crb = sigma_est if sigma_est.all() else sigma_est_aug
+        sigma_p_crb = sigma_p if sigma_p.all() else sigma_p_aug
+        CRB = cramer_rao_bound(M, U.T @ B @ U, sigma_crb**2,sigma_p, U.T @ sigma_theta @ U, N)
+        MSE_tests.append({
+            "method": 'cramer_rao_bound',
+            "N": N,
+            "SNR": SNR,
+            "MSE": CRB,
+        })
 
 import matplotlib.pyplot
-if two_phase_enabled:
-    print("MSE_two_phase_for_plot")
-    print(MSE_two_phase_for_plot)
-if two_phase_enabled:
+for N in points:
+    if two_phase_enabled:
+        MSE_two_phase = [x for x in MSE_tests if x['method']=='two_phase_topology' and N == x['N']]
+        MSE_two_phase_for_plot = [x['MSE'] for x in MSE_two_phase if N == x['N']]
+        fscore_for_plot = [x['F_score'] for x in MSE_two_phase if N == x['N']]
+
+    if augmented_enabled:
+        MSE_augmented_lagrangian = [x for x in MSE_tests if x['method']=='augmented_lagrangian' and N == x['N']]
+
+        MSE_augmented_lagrangian_for_plot = [x['MSE'] for x in MSE_augmented_lagrangian if N == x['N']]
+        fscore_augmented_lagrangian_for_plot = [x['F_score'] for x in MSE_augmented_lagrangian if N == x['N']]
+
+    CRBs = [x for x in MSE_tests if x['method']=='cramer_rao_bound' and N == x['N']]
+    CRBs_for_plot = [x['MSE'] for x in CRBs if  N == x['N']]
+
+
+    if two_phase_enabled:
+        print("MSE_two_phase_for_plot")
+        print(MSE_two_phase_for_plot)
+        print("fscore_for_plot")
+        print(fscore_for_plot)
+    if augmented_enabled:
+        print("MSE_augmented_lagrangian_for_plot")
+        print(MSE_augmented_lagrangian_for_plot)
+        print("fscore_augmented_lagrangian_for_plot")
+        print(fscore_augmented_lagrangian_for_plot)
     print("CRBs_for_plot")
     print(CRBs_for_plot)
-    print("fscore_for_plot")
-    print(fscore_for_plot)
-if augmented_enabled:
-    print("MSE_augmented_lagrangian_for_plot")
-    print(MSE_augmented_lagrangian_for_plot)
-    print("fscore_augmented_lagrangian_for_plot")
-    print(fscore_augmented_lagrangian_for_plot)
-if two_phase_enabled:
-    matplotlib.pyplot.plot(range_SNR, MSE_two_phase_for_plot)
+    if two_phase_enabled:
+        matplotlib.pyplot.plot(range_SNR, MSE_two_phase_for_plot)
+    if augmented_enabled:
+        matplotlib.pyplot.plot(range_SNR, MSE_augmented_lagrangian_for_plot)
     matplotlib.pyplot.plot(range_SNR, CRBs_for_plot)
-if augmented_enabled:
-    matplotlib.pyplot.plot(range_SNR, MSE_augmented_lagrangian_for_plot)
-matplotlib.pyplot.show()
+    matplotlib.pyplot.title("MSE for {}".format(str(N)))
+    matplotlib.pyplot.show()
+
+
+    # Now we do every plot in Grotas's paper
+    if two_phase_enabled:
+        B = [x for x in MSE_tests if x['method']=='two_phase_topology' and N == x['N'] and x["SNR"] == 15]
+        B = B[0]
+        snr = B['SNR']
+        B_matrix = B['B']
+        matplotlib.pyplot.matshow(B_matrix)
+        matplotlib.pyplot.title("B matrix, N={}, SNR={} dB".format(str(N), str(snr)))
+        matplotlib.pyplot.show()
+    if augmented_enabled:
+        B = [x for x in MSE_tests if x['method']=='augmented_lagrangian' and N == x['N'] and x["SNR"] == 15]
+        B = B[0]
+        snr = B['SNR']
+        B_matrix = B['B']
+        matplotlib.pyplot.matshow(B_matrix)
+        matplotlib.pyplot.title("B matrix, N={}, SNR={} dB".format(str(N), str(snr)))
+        matplotlib.pyplot.show()
+    matplotlib.pyplot.matshow(B_real)
+    matplotlib.pyplot.show()
